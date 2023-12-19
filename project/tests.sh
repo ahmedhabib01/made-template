c841fcda9287cbca3bc240da04da44599a88dbad
Ahmed Habib <72246938+ahmedhabib01@users.noreply.github.com>
2023-12-19 12:55:26 +0100

    Update tests.sh

--- project/tests.sh
+++ project/tests.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-python3 project/Project_Pipeline_Work3.py
+python Project_Pipeline_Work3.py
 echo "Pipeline Executed"
-python3 project/test.py
+python test.py
 echo "Pipeline Tested"
